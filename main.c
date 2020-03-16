73a190151314fe383f299c51d16e996645c0936a
KonstantinHlydov <61344850+KonstantinHlydov@users.noreply.github.com>
2020-03-16 14:01:39 +0300

    Хлудов Константин - Группа 1142 - Домашние задание

--- main.c
+++ main.c
@@ -3,52 +3,44 @@
 #include <conio.h>
 #define for_i for(int i = 0; i < r; i++)
 
-
-int summ(int *arr, int r, int c, int i)
+int summ(int* arr, int r, int c, int i)
 {
 	printf("Vvivod massiva:\n");
 	int f = 0;
-	for (int i = 0; i<c;i++)
+	for (int i = 0; i < c; i++)
 	{
 		for (int j = 0; j < c; j++)
 		{
 			f = f + *(arr + i + i * c + j);
 			*(arr + i + i * c + c) = f;
 		}
-	f = 0;
-	}	
-
-
-}
-
-
-void sort(int* arr, int r, int c, int i)
-{
-	int sch = 0;
-	for (int q = 0; q < c; q++)
-	{
-		for_i
-		{
-		for (int j = 1; j < r; j++)
-		{
-			if (*(arr + i+i * c + j) < *(arr + i+i * c + j - 1))
-			{
-				swap(&*(arr + i+i * c + j), &*(arr + i+i * c + j - 1));
-			}
-
-		}
-		}
+		f = 0;
 	}
 }
 
-void swap(int *a, int *b)
+void swap(int* a, int* b)
 {
 	int z = *a;
 	*a = *b;
 	*b = z;
 }
 
-
+void sort(int* arr, int r, int c, int i)
+{
+	for (int q = 0; q < c; q++)
+	{
+		for_i
+		{
+		for (int j = 1; j < r; j++)
+		{
+			if (*(arr + i + i * c + j) < *(arr + i + i * c + j - 1))
+			{
+				swap(&*(arr + i + i * c + j), &*(arr + i + i * c + j - 1));
+			}
+		}
+		}
+	}
+}
 
 int print(int* arr, int c, int r)
 {
@@ -68,42 +60,26 @@ int print(int* arr, int c, int r)
 	}
 }
 
-
-
-
-
 int main()
 {
 	int r = 3, c = 4;
 	int i = 0;
-	int sch;
 	int* arr = (int*)malloc(r * c * sizeof(int));
 	for_i
-		for (int j = 0; j < (c - 1); j++) 
+		for (int j = 0; j < (c - 1); j++)
 		{
 			*(arr + i * c + j) = rand() % 3;
 		}
-
 	for_i
 	{
 		int j = (c - 1);
 		*(arr + i * c + j) = 0;
 	}
-		
-
-		
-
-	print(&*arr,  c, r);
-
-	summ(&*arr, c, r, i);
-
 	print(&*arr, c, r);
-
-	sort( &*arr, c, r, i);
-
-    print(&*arr, c, r);
-
+	summ(&*arr, c, r, i);
+	print(&*arr, c, r);
+	sort(&*arr, c, r, i);
+	printf("Itog sortirovki:\n");
+	print(&*arr, c, r);
 	free(arr);
-
-
 }
